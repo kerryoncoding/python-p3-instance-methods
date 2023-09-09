6ec14ffcd524b56c042e6def98a0afaad17d59a5
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-09 10:38:03 -0400

    getting Dogs to bark with class

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,11 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
